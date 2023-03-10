0e03c2afedca8117957e4690ce89e255b10d257a
eugeniefroustey <eugenie.froustey@2moro.fr>
2023-03-10 11:14:17 +0100

    added example quest #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BlockComponent } from './block/block.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ROUTES } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent, BlockComponent, UserProfileComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    BlockComponent,
+    UserProfileComponent,
+    HomeComponent,
+    ContactComponent,
+  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent],
 })
